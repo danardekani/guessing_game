e4ac4bb2015e6f2703a8bf0a087c9797d397bd8c
Daniel <ardekanicapital@gmail.com>
2020-04-03 18:45:21 -0400

    Add break after user has answered correctly

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 
 	// return random number from createRandomInteger
 	createNumber := createRandomInteger(1, 100)
+	fmt.Println("the secret number is", createNumber)
 
 	// accept and read user input
 	var attempts int
@@ -26,25 +27,23 @@ func main() {
 		input = strings.TrimSuffix(input, "\n")
 
 		guess, err := strconv.Atoi(input)
-		}
-
+		
 		if err != nil {
-		fmt.Println("Invalid input. Please enter an integer value")
-		return
+			fmt.Println("Invalid input. Please enter an integer value")
+			return
 		}
-
 		// if/else statements comparing user input vs machine output
 		fmt.Println("Your guess is", guess)
 		if guess > createNumber {
-		fmt.Println("You're guess is too high. Try again.")
+			fmt.Println("You're guess is too high. Try again.")
 		} else if guess < createNumber {
-		fmt.Println("Your guess is too low. Try again. ")
+			fmt.Println("Your guess is too low. Try again. ")
 		} else {
-		fmt.Println("That is correct! Good job!")
+			fmt.Println("That is correct! Good job!")
+			break
 		}
 	}
 }
-
 func createRandomInteger(min, max int) int {
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(max-min) + min
